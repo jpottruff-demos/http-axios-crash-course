ad4a583a73624202a8f0302d5122bacccb70c35b
Jeff Pottruff <jeffpottruff@gmail.com>
2020-12-15 12:25:22 -0500

    ADD error handler and cancel functions

--- main.js
+++ main.js
@@ -120,70 +120,107 @@ function transformResponse() {
   axios(options).then(res => showOutput(res));
 }
   
-  // ERROR HANDLING
-  function errorHandling() {
-    console.log('Error Handling');
-  }
+// ERROR HANDLING
+function errorHandling() {
+  axios
+  .get('https://jsonplaceholder.typicode.com/todosssss', {
+    params: { _limit: 5 }
+  })
+  .then(res => showOutput(res))
+  .catch(err => {
+    if (err.response) {
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+    } else if (err.request) {
+      // eg. Request was made but there was no reponse
+      console.log(err.request);
+    } else {
+      console.error(err.message)
+    }
+  })
+}
   
-  // CANCEL TOKEN
-  function cancelToken() {
-    console.log('Cancel Token');
-  }
-  
-  // INTERCEPTING REQUESTS & RESPONSES
-  // NOTE: this message will show up in the console and then the function will continue on as expected
-  axios.interceptors.request.use(config => {
-    console.log(`${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime()}`);
-    return config;
-  }, error => {
-    return Promise.reject(error);
+// CANCEL TOKEN
+function cancelToken() {
+  const source = axios.CancelToken.source();
+
+  axios
+  .get('https://jsonplaceholder.typicode.com/todosssss', {
+    cancelToken: source.token
+  })
+  .then(res => showOutput(res))
+  .catch(thrown => {
+    if (axios.isCancel(thrown)) {
+      console.log('Request Canceled', thrown.message);
+    }
   });
-  
-  // AXIOS INSTANCES
-  
-  // Show output in browser
-  function showOutput(res) {
-    document.getElementById('res').innerHTML = `
-    <div class="card card-body mb-4">
-      <h5>Status: ${res.status}</h5>
-    </div>
-    <div class="card mt-3">
-      <div class="card-header">
-        Headers
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
-      </div>
-    </div>
-    <div class="card mt-3">
-      <div class="card-header">
-        Data
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.data, null, 2)}</pre>
-      </div>
-    </div>
-    <div class="card mt-3">
-      <div class="card-header">
-        Config
-      </div>
-      <div class="card-body">
-        <pre>${JSON.stringify(res.config, null, 2)}</pre>
-      </div>
-    </div>
-  `;
+
+  // NOTE: this would be something meaningful - not just true (eg. something happens that should trigger a cancel)
+  if (true) {
+    source.cancel('I canceled it')
   }
+}
   
-  // Event listeners
-  document.getElementById('get').addEventListener('click', getTodos);
-  document.getElementById('post').addEventListener('click', addTodo);
-  document.getElementById('update').addEventListener('click', updateTodo);
-  document.getElementById('delete').addEventListener('click', removeTodo);
-  document.getElementById('sim').addEventListener('click', getData);
-  document.getElementById('headers').addEventListener('click', customHeaders);
-  document
-    .getElementById('transform')
-    .addEventListener('click', transformResponse);
-  document.getElementById('error').addEventListener('click', errorHandling);
-  document.getElementById('cancel').addEventListener('click', cancelToken);
+// INTERCEPTING REQUESTS & RESPONSES
+// NOTE: this message will show up in the console and then the function will continue on as expected
+axios.interceptors.request.use(config => {
+  console.log(`${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime()}`);
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+  
+// AXIOS INSTANCES
+  
+
+
+
+
+
+// Show output in browser
+function showOutput(res) {
+  document.getElementById('res').innerHTML = `
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.status}</h5>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Headers
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+    </div>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Data
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.data, null, 2)}</pre>
+    </div>
+  </div>
+  <div class="card mt-3">
+    <div class="card-header">
+      Config
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.config, null, 2)}</pre>
+    </div>
+  </div>
+`;
+}
+  
+// Event listeners
+document.getElementById('get').addEventListener('click', getTodos);
+document.getElementById('post').addEventListener('click', addTodo);
+document.getElementById('update').addEventListener('click', updateTodo);
+document.getElementById('delete').addEventListener('click', removeTodo);
+document.getElementById('sim').addEventListener('click', getData);
+document.getElementById('headers').addEventListener('click', customHeaders);
+document
+  .getElementById('transform')
+  .addEventListener('click', transformResponse);
+document.getElementById('error').addEventListener('click', errorHandling);
+document.getElementById('cancel').addEventListener('click', cancelToken);
   
